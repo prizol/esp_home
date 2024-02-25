df66cf16c482d8b0461670f3ee6e4b02c40addb4
prizol <60255857+prizol@users.noreply.github.com>
2024-02-25 21:39:16 +0100

    Update __init__.py

--- components/fogymero_dal/__init__.py
+++ components/fogymero_dal/__init__.py
@@ -1,1 +1,20 @@
+import re
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import (
+    CONF_ID,
+    CONF_RECEIVE_TIMEOUT,
+    CONF_UPDATE_INTERVAL,
+)
+
+CODEOWNERS = ["@prizol"]
+
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["sensor", "text_sensor", "switch", "binary_sensor"]
+
+fogymero_dal_ns = cg.esphome_ns.namespace("fogymero_dal")
+FogymeroDalComponent = fogymero_dal_ns.class_(
+    "FogymeroDalComponent", cg.Component, uart.UARTDevice
+)
 
