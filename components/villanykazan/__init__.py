073e80c2337cb7213318f22235507885ac2d9eb6
prizol <60255857+prizol@users.noreply.github.com>
2024-03-03 16:22:35 +0100

    Update __init__.py

--- components/villanykazan/__init__.py
+++ components/villanykazan/__init__.py
@@ -17,15 +17,12 @@ CONF_I2 = "aram_2"
 CONF_I3 = "aram_3"
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(FogymeroDalComponent),
+    cv.GenerateID(): cv.declare_id(ElektromosKazanComponent),
+    cv.Optional(CONF_I1): sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
+    cv.Optional(CONF_I2): sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
+    cv.Optional(CONF_I13 sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
 
-    cv.Optional(CONF_I1):
-        sensor.sensor_schema(device_class=DEVICE_CLASS_CURRENT,unit_of_measurement=UNIT_AMPERE,accuracy_decimals=1,state_class=STATE_CLASS_MEASUREMENT).extend(),
-    cv.Optional(CONF_I2):
-        sensor.sensor_schema(device_class=DEVICE_CLASS_CURRENT,unit_of_measurement=UNIT_AMPERE,accuracy_decimals=1,state_class=STATE_CLASS_MEASUREMENT).extend(),
-    cv.Optional(CONF_I3):
-        sensor.sensor_schema(device_class=DEVICE_CLASS_CURRENT,unit_of_measurement=UNIT_AMPERE,accuracy_decimals=1,state_class=STATE_CLASS_MEASUREMENT).extend(),
-}).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
+}).extend(cv.polling_component_schema('60s'))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
