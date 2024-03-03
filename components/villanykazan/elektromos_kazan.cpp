76d01ee38aff2a6a1add58c0911c81f71b7fb209
prizol <60255857+prizol@users.noreply.github.com>
2024-03-03 17:09:48 +0100

    Update elektromos_kazan.cpp

--- components/villanykazan/elektromos_kazan.cpp
+++ components/villanykazan/elektromos_kazan.cpp
@@ -1,1 +1,38 @@
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+//#include "esphome/components/text_sensor/text_sensor.h"
+//#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/core/log.h"
+#include "fogymero_dal.h"
+
+namespace esphome {
+namespace elektromos_kazan {
+
+static const char *TAG = "fogymero_dal";
+
+void ElektromosKazanComponent::setup() {
+  // nothing to do here
+  
+}
+
+void ElektromosKazanComponent::loop() {
+
+
+  }
+
+
+
+void ElektromosKazanComponent::update() {
+  
+    ESP_LOGV(TAG, "update lefutott");
+   
+  }
+
+
+void ElektromosKazanComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "Fogymérő DAL");
+}
+
+} // namespace fogymerodal
+} // namespace esphome
 
